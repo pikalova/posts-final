d89aee794408cc88845f03467b1d69c35bc70839
pikalova <nata-g12@yandex.ru>
2022-04-14 19:18:53 +1200

    1404

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 import { Card as CardMui } from '@mui/material';
 
-import { Avatar, CardContent, CardHeader, Divider, IconButton } from '@mui/material';
+import { Avatar, CardContent, CardHeader, Divider } from '@mui/material';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCirclePlus } from '@fortawesome/free-solid-svg-icons';
+import { faCirclePlus, faCircleCheck } from '@fortawesome/free-solid-svg-icons';
 
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
@@ -19,24 +19,24 @@ const dateParse = (dateString) => {
 export const Card = ({ item }) => {
     return (
         <CardMui sx={{ maxWidth: 400 }}>
-            {console.log(item)}
             <CardContent>
                 <Button variant='text' style={{ minHeight: '80px', minWidth: '100%' }}>{item.title}</Button>
                 <Divider />
-                <CardHeader avatar={<Avatar alt={`${item.author.name}`} src={`$item.author.avatar`} />} title={item.author.name} />
+                <CardHeader avatar={<Avatar alt={item.author.name} src={item.author.avatar} />} title={item.author.name} />
                 <Typography style={{ minHeight: '120px' }} variant="body2" color="text.secondary">
                     {item.text}
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
                     Tags:
                     {item.tags.map((el) => {
-                        return <Button size="small">{el}</Button>
+                        return <Button size="small" key={el}>{el}</Button>
                     })}
                 </Typography>
                 <Typography variant="body2">
-                    <FontAwesomeIcon icon={faCirclePlus} /> {dateParse(item["created_at"])}
-                    <Divider orientation='vertical' style={{ margin: '0px 5px', height: 20 }}></Divider>
-                    <FontAwesomeIcon icon={faCirclePlus} /> {dateParse(item["created_at"])}
+                    <FontAwesomeIcon icon={faCirclePlus} color="orange" /> {dateParse(item["created_at"])}
+                </Typography>
+                <Typography variant="body2">
+                    <FontAwesomeIcon icon={faCircleCheck} color='green' /> Last Update: {dateParse(item["updated_at"])}
                 </Typography>
 
             </CardContent>
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,11 +2,15 @@ import React from 'react'
 import style from './style.module.css';
 import Button from '@mui/material/Button';
 
+const addPost= () => {
+    console.log('Ecть контакт!!!');
+}
+
 export const Menu = () => {
     return (
         <div className={style.menu}>
             <div className={style.buttonMenu}>
-                <Button variant="contained" onClick={console.log('Есть контакт')}>Add post</Button>
+                <Button variant="contained" onClick={addPost} >Add post</Button>
             </div>
         </div>
     )
